5447d819537ee51db6e71e274c39c7e5cb53dff4
Yves <2316041084@qq.com>
2018-12-19 09:42:28 +0800

    代码更新

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,21 +1,30 @@
 
 var config = require("../config");
+//获取config配置文件 ctrl+鼠标点击可跳入
 
 if(!process.env.NODE_ENV){
+    console.log(process)    //node ./dev-server.js  node环境运行文件可以看到console
+    //创建全局process对象
     process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
+    //全局process对象，NODE_ENV 全局用于判断是生产或者是开发环境。
 }
 
+//引入组件
 const opn = require('opn');
 const path = require('path');
 const webpack = require('webpack');
 const express = require('express');
 const webpackConfig = require('./webpack.dev');
-
+//引入组件
 
 const port = process.env.PORT || config.dev.port;
+//port
 const autoOpenBrowser = !!config.dev.autoOpenBrowser;
+//浏览器自动开启
 const proxyTable = config.dev.proxyTable;
+//代理
 
 const app = express();
 var compiler = webpack(webpackConfig);
 
+console.log(compiler);
--- config/index.js
+++ config/index.js
@@ -1,13 +1,21 @@
 module.exports = {
     dev:{
+        //dev开发环境配置
         env:require("./dev.env.js"),
+        //env添加在node全局对象process中 用于全局标识为 dev开发环境
         port:1888,
+        //端口
         autoOpenBrowser:true,
+        //是否从浏览器打开
         assetsSubDirectory: 'static',
+        //静态文件夹
         assetsPublicPath: '/',
+        //公共路径
         proxyTable:{}
+        //服务器代理
     },
     build:{
+        //build生产环境配置
         env:require("./prod.env.js"),
     }
 }
